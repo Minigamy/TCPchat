1653eaefbb471dda3e12ebbd4b3fb4d7a77c190e
nikita <mineev.n.s@mail.ru>
2022-04-11 01:35:16 +0300

    Start!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='chat',
+    name='tcpchat',
     version='0.0.1',
     packages=find_packages(),
     scripts=['chat/server', 'chat/client'],
